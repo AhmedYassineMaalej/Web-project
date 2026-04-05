162bbae52555cdc9f9b662fafc0f33a6b8ad1d5a
AhmedYassineMaalej <ahmedyessinemaalej@gmail.com>
2026-04-05 15:42:40 +0100

    code cleanup

--- ProductExtractor/shared_types.py
+++ ProductExtractor/shared_types.py
@@ -2,4 +2,3 @@ import selenium.webdriver.chrome.webdriver as chrome
 import selenium.webdriver.firefox.webdriver as firefox
 
 type WebDriver = chrome.WebDriver | firefox.WebDriver
-type Point = tuple[float, float]
